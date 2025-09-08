d9b8ae43c5a114c3b394403f32f5bcb270a295c0
Rasool <rasoolol.77@gmail.com>
2025-09-08 17:15:25 +0330

    feat: install prisma and mysql for database

---
+++ prisma/seed.ts
@@ -0,0 +1,20 @@
+import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcryptjs';
+
+const db = new PrismaClient();
+
+async function main() {
+  const hashed = await bcrypt.hash('1234', 10);
+  await db.user.upsert({
+    where: { email: 'admin@test.com' },
+    update: {},
+    create: {
+      email: 'admin@test.com',
+      password: hashed,
+      name: 'Rasool',
+      role: 'admin',
+    },
+  });
+}
+
+main().finally(() => db.$disconnect());
