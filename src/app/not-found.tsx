187e5544758cd3b2de803954abfe1e81ec41e2c3
Rasool <rasoolol.77@gmail.com>
2025-09-10 22:28:03 +0330

    feat: add not found page to app

---
+++ src/app/not-found.tsx
@@ -0,0 +1,15 @@
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+
+function NotFoundPage() {
+  return (
+    <div className="flex flex-col items-center justify-center gap-3 h-full w-full">
+      this page does not exist!
+      <Button asChild>
+        <Link href="/">go back to home page</Link>
+      </Button>
+    </div>
+  );
+}
+
+export default NotFoundPage;
