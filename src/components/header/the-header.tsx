bda31d0fe80a635a87f097e1f3e4210c2757ea8b
Rasool <rasoolol.77@gmail.com>
2025-09-12 14:01:57 +0330

    feat: install sheet and make nav responsive

--- src/components/header/the-header.tsx
+++ src/components/header/the-header.tsx
@@ -1,57 +1,16 @@
-'use client';
-
-import { Button } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
-import { Loader, LogOutIcon, ShoppingCartIcon, UserIcon } from 'lucide-react';
-import { signOut, useSession } from 'next-auth/react';
 import Link from 'next/link';
-import { ToggleTheme } from './toggle-theme';
+import HeaderMenu from './header-menu';
 
 export function SiteHeader({ className }: { className: string }) {
-  const { data: session, status } = useSession();
-
   return (
     <header className={cn('border-b', className)}>
       <div className="container mx-auto flex h-14 items-center justify-between px-4">
         <Link href="/" className="font-semibold tracking-tight">
-          Next Panel
+          Store Project
         </Link>
-        <nav className="flex items-center gap-2">
-          <ToggleTheme />
 
-          <Button variant="ghost" asChild>
-            <Link
-              href="/cart"
-              className="flex gap-2  text-gray-600 hover:text-gray-900 dark:text-gray-300 dark:hover:text-white"
-            >
-              <ShoppingCartIcon />
-              Cart
-            </Link>
-          </Button>
-
-          {status === 'loading' ? (
-            <Button disabled>
-              <Loader className="animate-spin" />
-              Loading
-            </Button>
-          ) : session ? (
-            <Button
-              variant="outline"
-              className="bg-red-200"
-              onClick={() => signOut({ callbackUrl: '/auth' })}
-            >
-              <LogOutIcon />
-              Logout
-            </Button>
-          ) : (
-            <Button asChild>
-              <Link href="/auth">
-                <UserIcon />
-                Login
-              </Link>
-            </Button>
-          )}
-        </nav>
+        <HeaderMenu />
       </div>
     </header>
   );
