bf352418709c710dbfa46c181e348640f0d7a2ca
Rasool <rasoolol.77@gmail.com>
2025-10-10 22:40:35 +0330

    feat: add users page to show users list

---
+++ src/app/users/page.tsx
@@ -0,0 +1,20 @@
+import { getUsers } from '@/actions/user-actions';
+
+export default async function UsersPage() {
+  const users = await getUsers();
+
+  if (!users || users.length === 0) return <p>no user found</p>;
+
+  return (
+    <div>
+      <h1>Users</h1>
+      <ul>
+        {users.map((u) => (
+          <li key={u.id}>
+            {u.name} {u.email}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
