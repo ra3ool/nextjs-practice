c3b7d501c261b94bc07c57fa7857c4c3ab78f310
Rasool <rasoolol.77@gmail.com>
2025-10-15 09:55:51 +0330

    perf: update the cart-table

--- src/app/cart/page.tsx
+++ src/app/cart/page.tsx
@@ -1,4 +1,5 @@
 import { getMyCart } from '@/actions/cart.actions';
+import { CartInfo } from '@/components/cart/cart-info';
 import { CartTable } from '@/components/cart/cart-table';
 import { CartType } from '@/types/cart.type';
 import { serializeCart } from '@/utils/serialize-cart';
@@ -13,7 +14,14 @@ export default async function CartPage() {
 
   return (
     <>
-      <CartTable cart={cart} />
+      <h2 className="text-2xl">shopping cart</h2>
+      {!cart || cart.items?.length === 0 ? (
+        <p className="mt-3">cart is empty, go shopping</p>
+      ) : (
+        <div className="grid grid-cols-1 lg:grid-cols-4 gap-5 mt-3">
+          <CartTable cart={cart} className="lg:col-span-3" />
+        </div>
+      )}
     </>
   );
 }
--- src/components/cart/cart-table.tsx
+++ src/components/cart/cart-table.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { AddToCart } from '@/components/products/add-to-cart';
 import {
   Table,
   TableBody,
@@ -9,71 +10,70 @@ import {
   TableHeader,
   TableRow,
 } from '@/components/ui/table';
+import { cn } from '@/lib/utils';
 import { CartType } from '@/types/cart.type';
 import { round2 } from '@/utils/round2';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
-import { AddToCart } from '../products/add-to-cart';
 
-function CartTable({ cart }: { cart?: CartType }) {
+function CartTable({
+  cart,
+  className,
+}: {
+  cart: CartType;
+  className?: string;
+}) {
   const router = useRouter();
   const goToProductDetails = (itemSlug: string) => {
     router.push(`/product/${itemSlug}`);
   };
 
   return (
-    <>
-      <h2 className="text-2xl">shopping cart</h2>
-      {!cart || cart.items?.length === 0 ? (
-        <div className="">cart is empty, go shopping</div>
-      ) : (
-        <div className="grid grid-cols-1 lg:grid-cols-4 lg:gap-5">
-          <div className="overflow-x-auto lg:col-span-3">
-            <Table>
-              <TableHeader>
-                <TableRow>
-                  <TableHead>Item</TableHead>
-                  <TableHead>Quantity</TableHead>
-                  <TableHead>Price</TableHead>
-                </TableRow>
-              </TableHeader>
-              <TableBody>
-                {cart.items.map((item) => (
-                  <TableRow key={item.slug}>
-                    <TableCell
-                      className="font-medium flex items-center gap-4 cursor-pointer"
-                      onClick={() => goToProductDetails(item.slug)}
-                    >
-                      <div className="relative h-10 w-10">
-                        <Image
-                          src={item.image}
-                          alt={item.name}
-                          fill
-                          sizes="40px"
-                          className="object-contain"
-                        />
-                      </div>
-                      {item.name}
-                    </TableCell>
-                    <TableCell>
-                      <AddToCart
-                        cart={cart}
-                        item={{
-                          ...item,
-                          qty: 1,
-                        }}
-                      />
-                    </TableCell>
-                    <TableCell>{round2(item.price * item.qty)}</TableCell>
-                  </TableRow>
-                ))}
-              </TableBody>
-              <TableCaption>your cart items and details</TableCaption>
-            </Table>
-          </div>
-        </div>
-      )}
-    </>
+    <div className={cn(className)}>
+      <div className="overflow-x-auto">
+        <Table>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Item</TableHead>
+              <TableHead>Quantity</TableHead>
+              <TableHead>Price</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {cart.items.map((item) => (
+              <TableRow key={item.slug}>
+                <TableCell
+                  className="font-medium flex items-center gap-4 cursor-pointer"
+                  onClick={() => goToProductDetails(item.slug)}
+                >
+                  <div className="relative h-10 w-10">
+                    <Image
+                      src={item.image}
+                      alt={item.name}
+                      fill
+                      sizes="40px"
+                      className="object-contain"
+                    />
+                  </div>
+                  {item.name}
+                </TableCell>
+                <TableCell>
+                  <AddToCart
+                    cart={cart}
+                    item={{
+                      ...item,
+                      qty: 1,
+                    }}
+                  />
+                </TableCell>
+                <TableCell>${round2(item.price * item.qty)}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+          <TableCaption>your cart items and details</TableCaption>
+        </Table>
+      </div>
+    </div>
   );
 }
 
